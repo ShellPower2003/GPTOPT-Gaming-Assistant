2e48d8b4de47f0560492cebf7a488dba233d77b7
ShellPower2003 <210625166+ShellPower2003@users.noreply.github.com>
2026-05-25 02:04:52 -0500

    Launch clean GPTOPT app GUI from control launcher

--- App/GPTOPT-Control.cmd
+++ App/GPTOPT-Control.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0.."
-powershell.exe -NoProfile -NoExit -File ".\Run-GPTOPT.ps1" -Mode menu
+powershell.exe -NoProfile -NoExit -File ".\Scripts\Invoke-GPTOPTAppGUI.ps1"
 exit /b %ERRORLEVEL%
